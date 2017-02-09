2edb1ef2823adf16fb34bedd86eaf76c22f99c24
sidd4848 <ranjansiddharth484848@gmail.com>
2017-02-09 20:02:15 +0530

    coustomisation1

--- demo1.js
+++ demo1.js
@@ -1,13 +1,18 @@
 window.onload = function(){
-	var canvas = document.getElementById("space");
-	var ctx = canvas.getContext("2d");
 	
 }
 var set;
 function jalao(set){
-	if(set === 1)
-	document.getElementById("imageBulb").src = "glow.svg";
-    else 
-		if(set === 2)
-			document.getElementById("imagefan").src = "fanchalao.gif";
+     switch(set){
+	 case 1 : document.getElementById("imageBulb").src = "glow.svg"; break;
+	 case 2 : document.getElementById("imageBulb").src = "bujhahua.svg"; break;
+	 case 3 : document.getElementById("imagefan").src = "fanchalao.gif"; break;
+	 case 4 : document.getElementById("imagefan").src = "FAN.png"; break;
+	 case 5 : document.getElementById("imageBulb1").src = "glow.svg"; break;
+	 case 6 : document.getElementById("imageBulb1").src = "bujhahua.svg"; break;
+	 case 7 : document.getElementById("imageBulb2").src = "glow.svg"; break;
+	 case 8 : document.getElementById("imageBulb2").src = "bujhahua.svg"; break;
+	 case 9 : document.getElementById("imagefan1").src = "fanchalao.gif"; break;
+	 case 10 : document.getElementById("imagefan1").src = "FAN.png"; break;
+	 }
 }
